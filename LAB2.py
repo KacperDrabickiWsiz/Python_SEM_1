4456b880bf315348a44dba1d2712f43a8acc8e63
KacperDrabickiWsiz <w68520@student.wsiz.edu.pl>
2022-10-23 10:42:52 +0200

    Lab

---
+++ LAB2.py
@@ -0,0 +1,43 @@
+#ZADANIE 1
+'''a=int(input('Podaj wiek: '))
+
+if a<4:
+    cena=0
+elif a<=18:
+    cena=10
+else: cena=20
+print(f'Cena= {cena}zł')'''
+
+#ZADANIE 2
+
+print("""
+1) dodawanie
+2) odejmowanie
+3) mnożenie
+4) dzielenie 
+5) potęgowanie """)
+
+z=int(input('Jaką operacje chcesz wykoań'))
+if z>=6 or z<=0:
+    print('Niepoprawna wartość')
+    exit()
+
+x=float(input('Podaj wartość 1: '))
+y=float(input('Podaj wartość 2: '))
+
+if z==1:
+    w=x+y
+elif z==2:
+    w=x-y
+elif z==3:
+    w=x*y
+elif z==4:
+    w=x/y
+elif z==5:
+    w=x**y
+
+
+
+print(f'Wynik = {w}')
+
+
