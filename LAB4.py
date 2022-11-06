e573c45264f27973edb56cbf2b70cba097dbbe7a
KacperDrabickiWsiz <w68520@student.wsiz.edu.pl>
2022-11-06 13:52:13 +0100

    Lab4

--- LAB4.py
+++ LAB4.py
@@ -1,1 +1,6 @@
-L = [123, 'abc', 1.23, {}]''
+lista_zakupow=['produkt1','produkt2','produkt3','produkt4','produkt5']
+print(lista_zakupow)
+print(lista_zakupow[0]+","+lista_zakupow[1])
+lista_zakupow[2]='mleko'
+print(lista_zakupow)
+print(lista_zakupow[-2]+","+lista_zakupow[-1]) #-2 i -1 to ostatni i przedostati element listy
