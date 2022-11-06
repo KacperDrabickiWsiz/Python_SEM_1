9deb9eee58b9834c0906f5e48a22cacb96cdeb68
KacperDrabickiWsiz <w68520@student.wsiz.edu.pl>
2022-11-06 13:30:08 +0100

    Lab

--- LAB3_7.py
+++ LAB3_7.py
@@ -1,5 +1,5 @@
-#ZADANIE 1
-for a in range(1,101):
+#ZADANIE 7
+'''for a in range(1,101):
     print(a, end=' ') #zapis w 1 lini END=''
 print('')
 for b in range(100,0,-1):
@@ -9,6 +9,11 @@ for c in range(7,84,7):
     print(c, end=' ')
 print('')
 for d in range(20,0,-2):
-    print(d, end=' ')
+    print(d, end=' ')'''
 
-#ZADANIE 2
+#ZADANIE 8
+a=int(input('Podaj ilość wierszy:'))
+for c in range(a):
+    for b in range(a):
+        print('*', end='')
+    print('')
