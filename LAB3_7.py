f8cfea7efa1bde7a1b102a5f7a89342aa9cd89be
KacperDrabickiWsiz <w68520@student.wsiz.edu.pl>
2022-11-06 13:10:38 +0100

    Lab

---
+++ LAB3_7.py
@@ -0,0 +1,14 @@
+#ZADANIE 1
+for a in range(1,101):
+    print(a, end=' ') #zapis w 1 lini END=''
+print('')
+for b in range(100,0,-1):
+    print(b, end=' ')
+print('')
+for c in range(7,84,7):
+    print(c, end=' ')
+print('')
+for d in range(20,0,-2):
+    print(d, end=' ')
+
+#ZADANIE 2
