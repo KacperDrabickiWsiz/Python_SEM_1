b11509170e5b680d0768767451c0fbec6e809cd2
KacperDrabickiWsiz <116463063+KacperDrabickiWsiz@users.noreply.github.com>
2023-01-15 13:37:16 +0100

    Add files via upload

--- LAB6.py
+++ LAB6.py
@@ -27,10 +27,11 @@ print(lista_indeksow)
 '''
 
 def sum_of_values(dict1):
-    suma = 0
-    for wartosc in dict1.values():
-        suma += wartosc
-    return suma
-
+    wynik = 0
+    for i in dict1.values():
+        wynik += i
+    return wynik
 dict1 = {'data1':10, 'data2':-4, 'data3':2}
+dict2 = {'data1':15, 'data2':-2, 'data3':1, 'data4':87}
 print(sum_of_values(dict1))
+print(sum_of_values(dict2))
