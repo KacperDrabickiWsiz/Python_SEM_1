7cf69bd07b98a0ee9be97bde4e044157422e93bc
KacperDrabickiWsiz <116463063+KacperDrabickiWsiz@users.noreply.github.com>
2023-01-15 14:02:04 +0100

    Add files via upload

--- LAB6.py
+++ LAB6.py
@@ -24,7 +24,6 @@ l1 = [1, 2, 1, 1, 3, 5]
 wartosc_szukana = 1
 lista_indeksow = find(l1, wartosc_szukana)
 print(lista_indeksow)
-'''
 
 def sum_of_values(dict1):
     wynik = 0
@@ -35,3 +34,15 @@ dict1 = {'data1':10, 'data2':-4, 'data3':2}
 dict2 = {'data1':15, 'data2':-2, 'data3':1, 'data4':87}
 print(sum_of_values(dict1))
 print(sum_of_values(dict2))
+
+def potęga(lista1, lista2):
+    if len(lista1) != len(lista2):
+        return "Listy muszą mieć taką samą długość"
+    L3 = []
+    for i in range(len(lista1)):
+        L3.append(lista1[i] ** lista2[i])
+    return L3
+
+wynik=potęga([2,4,3,7],[2,4,6,1])
+print(wynik)
+'''
